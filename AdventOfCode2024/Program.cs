ed5d03fc64aaa7c3ba19e880db158b492fd5427e
lmanja <ledianmanja19@gmail.com>
2024-12-01 21:43:38 +0100

    day1 done

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AdventOfCode2024;
 
-Console.WriteLine("Hello, World!");
+List<int> list1 = [3, 4, 2, 1, 3, 3]; 
+List<int> list2 = [4, 3, 5, 3, 9, 3];
+
+Console.Write(Day1.GetSum(list1, list2));
